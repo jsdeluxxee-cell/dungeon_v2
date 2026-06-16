cf0e6dcafdc14423ad20eab55378fb61b5b16931
Kainoa <jsdeluxxee@gmail.com>
2026-06-15 16:53:07 -1000

    Clean repo and add gitignore

--- infinite.py
+++
@@ -1,5 +0,0 @@
-countdown = 5
-
-
-while countdown > 0:
-    print("Stuck!")
--- loop_basics.py
+++
@@ -1,8 +0,0 @@
-# Counting down to match start
-countdown = 5
-
-while countdown > 0:
-    print(f"Match starting in {countdown}...")
-    countdown = countdown - 1
-
-print("GO!")
